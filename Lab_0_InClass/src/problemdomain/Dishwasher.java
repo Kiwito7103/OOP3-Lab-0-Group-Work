913bb9c537a06fbce2286e7a9016bd59e693434d
Kiwito <lyubstriletskyy@gmail.com>
2025-09-17 10:40:08 -0600

    hi, i made the tostring messages for each file, and started the manager

--- Lab_0_InClass/src/problemdomain/Dishwasher.java
+++ Lab_0_InClass/src/problemdomain/Dishwasher.java
@@ -4,6 +4,7 @@ public class Dishwasher extends Appliance
 {
 	private String feature;
 	private String soundRating;
+
 	/**
 	 * @param itemNumber
 	 * @param brand
@@ -15,34 +16,49 @@ public class Dishwasher extends Appliance
 	 * @param soundRating
 	 */
 	public Dishwasher(int itemNumber, String brand, int quantity, int wattage, String color, double price,
-			String feature, String soundRating) {
+			String feature, String soundRating)
+	{
 		super(itemNumber, brand, quantity, wattage, color, price);
 		this.feature = feature;
 		this.soundRating = soundRating;
 	}
-	
+
 	/**
 	 * @return the feature
 	 */
-	public String getFeature() {
+	public String getFeature()
+	{
 		return feature;
 	}
+
 	/**
 	 * @param feature the feature to set
 	 */
-	public void setFeature(String feature) {
+	public void setFeature(String feature)
+	{
 		this.feature = feature;
 	}
+
 	/**
 	 * @return the soundRating
 	 */
-	public String getSoundRating() {
+	public String getSoundRating()
+	{
 		return soundRating;
 	}
+
 	/**
 	 * @param soundRating the soundRating to set
 	 */
-	public void setSoundRating(String soundRating) {
+	public void setSoundRating(String soundRating)
+	{
 		this.soundRating = soundRating;
 	}
+	
+	@Override
+	public String toString() {
+	    return "Dishwasher " super.toString() + ", " + getFeature() + ", " + getSoundRating();
+	}
+
+
 }
